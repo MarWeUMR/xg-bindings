f8224b5732adf3799b27e5fff0764d6b4d040fb4
marweumr <marcus.wallau@googlemail.com>
2022-05-29 22:13:49 +0200

    increment branch

--- src/booster.rs
+++ src/booster.rs
@@ -1,5 +1,6 @@
 use crate::dmatrix::DMatrix;
 use crate::error::XGBError;
+use indexmap::IndexMap;
 use libc;
 use std::collections::{BTreeMap, HashMap};
 use std::io::{self, BufRead, BufReader, Write};
@@ -7,7 +8,6 @@ use std::os::unix::ffi::OsStrExt;
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
 use std::{ffi, fmt, fs::File, ptr, slice};
-use indexmap::IndexMap;
 use tempfile;
 use xgboost_bib;
 
@@ -128,6 +128,93 @@ impl Booster {
         ))?;
         Ok(Booster { handle })
     }
+
+    pub fn train_increment(params: &TrainingParameters, model_name: &str) -> XGBResult<Self> {
+        let cached_dmats = {
+            let mut dmats = vec![params.dtrain];
+            if let Some(eval_sets) = params.evaluation_sets {
+                for (dmat, _) in eval_sets {
+                    dmats.push(*dmat);
+                }
+            }
+            dmats
+        };
+
+        let path = Path::new(model_name);
+        let bytes = std::fs::read(&path).expect("read saved booster file");
+        let mut bst = Booster::load_buffer(&bytes[..]).expect("load booster from buffer");
+        // let mut bst = Booster::new_with_cached_dmats(&params.booster_params, &cached_dmats)?;
+        //let num_parallel_tree = 1;
+
+        // load distributed code checkpoint from rabit
+        let version = bst.load_rabit_checkpoint()?;
+        debug!("Loaded Rabit checkpoint: version={}", version);
+        assert!(unsafe { xgboost_bib::RabitGetWorldSize() != 1 || version == 0 });
+
+        let _rank = unsafe { xgboost_bib::RabitGetRank() };
+        let start_iteration = version / 2;
+        //let mut nboost = start_iteration;
+
+        for i in start_iteration..params.boost_rounds as i32 {
+            // distributed code: need to resume to this point
+            // skip first update if a recovery step
+            if version % 2 == 0 {
+                if let Some(objective_fn) = params.custom_objective_fn {
+                    debug!("Boosting in round: {}", i);
+                    bst.update_custom(params.dtrain, objective_fn)?;
+                } else {
+                    debug!("Updating in round: {}", i);
+                    bst.update(params.dtrain, i)?;
+                }
+                bst.save_rabit_checkpoint()?;
+            }
+
+            assert!(unsafe {
+                xgboost_bib::RabitGetWorldSize() == 1
+                    || version == xgboost_bib::RabitVersionNumber()
+            });
+
+            //nboost += 1;
+
+            if let Some(eval_sets) = params.evaluation_sets {
+                let mut dmat_eval_results = bst.eval_set(eval_sets, i)?;
+
+                if let Some(eval_fn) = params.custom_evaluation_fn {
+                    let eval_name = "custom";
+                    for (dmat, dmat_name) in eval_sets {
+                        let margin = bst.predict_margin(dmat)?;
+                        let eval_result = eval_fn(&margin, dmat);
+                        let eval_results = dmat_eval_results
+                            .entry(eval_name.to_string())
+                            .or_insert_with(IndexMap::new);
+                        eval_results.insert(dmat_name.to_string(), eval_result);
+                    }
+                }
+
+                // convert to map of eval_name -> (dmat_name -> score)
+                let mut eval_dmat_results = BTreeMap::new();
+                for (dmat_name, eval_results) in &dmat_eval_results {
+                    for (eval_name, result) in eval_results {
+                        let dmat_results = eval_dmat_results
+                            .entry(eval_name)
+                            .or_insert_with(BTreeMap::new);
+                        dmat_results.insert(dmat_name, result);
+                    }
+                }
+
+                print!("[{}]", i);
+                for (eval_name, dmat_results) in eval_dmat_results {
+                    for (dmat_name, result) in dmat_results {
+                        print!("\t{}-{}:{}", dmat_name, eval_name, result);
+                    }
+                }
+                println!();
+            }
+        }
+
+        Ok(bst)
+    }
+
 
     /// Convenience function for creating/training a new Booster.
     ///
@@ -909,7 +996,7 @@ mod tests {
 
         let test_metrics = booster.evaluate(&dmat_test).unwrap();
         assert!(*test_metrics.get("logloss").unwrap() - 0.00692 < eps);
-        assert!(*test_metrics.get("map@4-").unwrap() -  0.005155 < eps);
+        assert!(*test_metrics.get("map@4-").unwrap() - 0.005155 < eps);
 
         let v = booster.predict(&dmat_test).unwrap();
         assert_eq!(v.len(), dmat_test.num_rows());
@@ -1081,112 +1168,198 @@ mod tests {
         assert_eq!(Booster::parse_eval_string(s, &["train", "test"]), metrics);
     }
 
-//     #[test]
-//     fn dump_model() {
-//         let dmat_train = DMatrix::load("xgboost-bib/xgboost/demo/data/agaricus.txt.train").unwrap();
-//
-//         println!("{:?}", dmat_train.shape());
-//
-//         let tree_params = tree::TreeBoosterParametersBuilder::default()
-//             .max_depth(2)
-//             .eta(1.0)
-//             .build()
-//             .unwrap();
-//         let learning_params = learning::LearningTaskParametersBuilder::default()
-//             .objective(learning::Objective::BinaryLogistic)
-//             .build()
-//             .unwrap();
-//         let booster_params = parameters::BoosterParametersBuilder::default()
-//             .booster_type(parameters::BoosterType::Tree(tree_params))
-//             .learning_params(learning_params)
-//             .verbose(false)
-//             .build()
-//             .unwrap();
-//
-//         let training_params = parameters::TrainingParametersBuilder::default()
-//             .booster_params(booster_params)
-//             .dtrain(&dmat_train)
-//             .boost_rounds(10)
-//             .build()
-//             .unwrap();
-//         let booster = Booster::train(&training_params).unwrap();
-//
-//         let features = FeatureMap::from_file("xgboost-bib/xgboost/demo/data/featmap.txt")
-//             .expect("failed to parse feature map file");
-//
-//         assert_eq!(
-//             booster.dump_model(true, Some(&features)).unwrap(),
-//             "0:[odor=none] yes=2,no=1,gain=4000.53101,cover=1628.25
-// 1:[stalk-root=club] yes=4,no=3,gain=1158.21204,cover=924.5
-// 		3:leaf=1.71217716,cover=812
-// 		4:leaf=-1.70044053,cover=112.5
-// 2:[spore-print-color=green] yes=6,no=5,gain=198.173828,cover=703.75
-// 		5:leaf=-1.94070864,cover=690.5
-// 		6:leaf=1.85964918,cover=13.25
-//
-// 0:[stalk-root=rooted] yes=2,no=1,gain=832.545044,cover=788.852051
-// 1:[odor=none] yes=4,no=3,gain=569.725098,cover=768.389709
-// 		3:leaf=0.78471756,cover=458.936859
-// 		4:leaf=-0.968530357,cover=309.45282
-// 	2:leaf=-6.23624468,cover=20.462389
-//
-// 0:[ring-type=pendant] yes=2,no=1,gain=368.744568,cover=457.069458
-// 1:[stalk-surface-below-ring=scaly] yes=4,no=3,gain=226.33696,cover=221.051468
-// 		3:leaf=0.658725023,cover=212.999451
-// 		4:leaf=5.77228642,cover=8.05200672
-// 2:[spore-print-color=purple] yes=6,no=5,gain=258.184265,cover=236.018005
-// 		5:leaf=-0.791407049,cover=233.487625
-// 		6:leaf=-9.421422,cover=2.53038669
-//
-// 0:[odor=foul] yes=2,no=1,gain=140.486069,cover=364.119354
-// 1:[gill-size=broad] yes=4,no=3,gain=139.860504,cover=274.101959
-// 		3:leaf=0.614153326,cover=95.8599854
-// 		4:leaf=-0.877905607,cover=178.241974
-// 	2:leaf=1.07747853,cover=90.0174103
-//
-// 0:[spore-print-color=green] yes=2,no=1,gain=112.605011,cover=189.202194
-// 1:[gill-spacing=close] yes=4,no=3,gain=66.4029999,cover=177.771835
-// 		3:leaf=-1.26934469,cover=42.277401
-// 		4:leaf=0.152607277,cover=135.494431
-// 	2:leaf=2.92190909,cover=11.4303684
-//
-// 0:[odor=almond] yes=2,no=1,gain=52.5610275,cover=170.612762
-// 1:[odor=anise] yes=4,no=3,gain=67.3869553,cover=150.881165
-// 		3:leaf=0.431742132,cover=131.902222
-// 		4:leaf=-1.53846073,cover=18.9789505
-// 2:[gill-spacing=close] yes=6,no=5,gain=12.4420624,cover=19.731596
-// 		5:leaf=-3.02413678,cover=3.65769386
-// 		6:leaf=-1.02315068,cover=16.0739021
-//
-// 0:[odor=none] yes=2,no=1,gain=66.2389145,cover=142.360611
-// 1:[odor=anise] yes=4,no=3,gain=31.2294312,cover=72.7557373
-// 		3:leaf=0.777142286,cover=64.5309982
-// 		4:leaf=-1.19710124,cover=8.22473907
-// 2:[spore-print-color=green] yes=6,no=5,gain=12.1987419,cover=69.6048737
-// 		5:leaf=-0.912605286,cover=66.1211166
-// 		6:leaf=0.836115122,cover=3.48375821
-//
-// 0:[gill-size=broad] yes=2,no=1,gain=20.6531773,cover=79.4027634
-// 1:[spore-print-color=white] yes=4,no=3,gain=16.0703697,cover=34.9289207
-// 		3:leaf=-0.0180106498,cover=25.0319824
-// 		4:leaf=1.4361918,cover=9.89693928
-// 2:[odor=foul] yes=6,no=5,gain=22.1144333,cover=44.4738464
-// 		5:leaf=-0.908311546,cover=36.982872
-// 		6:leaf=0.890622675,cover=7.49097395
-//
-// 0:[odor=almond] yes=2,no=1,gain=11.7128553,cover=53.3251991
-// 1:[ring-type=pendant] yes=4,no=3,gain=12.546154,cover=44.299942
-// 		3:leaf=-0.515293062,cover=15.7899179
-// 		4:leaf=0.56883812,cover=28.5100231
-// 	2:leaf=-1.01502442,cover=9.02525806
-//
-// 0:[population=clustered] yes=2,no=1,gain=14.8892794,cover=45.9312019
-// 1:[odor=none] yes=4,no=3,gain=10.1308851,cover=43.0564575
-// 		3:leaf=0.217203051,cover=22.3283749
-// 		4:leaf=-0.734555721,cover=20.7280827
-// 2:[stalk-root=missing] yes=6,no=5,gain=19.3462334,cover=2.87474418
-// 		5:leaf=3.63442755,cover=1.34154534
-// 		6:leaf=-0.609474957,cover=1.53319895
-// "
-//         );
+    #[test]
+    fn increment() {
+        let dmat_train = DMatrix::load("boston.csv?format=csv").unwrap();
+        let dmat_test = DMatrix::load("boston.csv?format=csv").unwrap();
+
+        let tree_params = tree::TreeBoosterParametersBuilder::default()
+            .max_depth(2)
+            .eta(1.0)
+            .build()
+            .unwrap();
+        let learning_params = learning::LearningTaskParametersBuilder::default()
+            .objective(learning::Objective::BinaryLogistic)
+            .eval_metrics(learning::Metrics::Custom(vec![
+                learning::EvaluationMetric::MAPCutNegative(4),
+                learning::EvaluationMetric::LogLoss,
+                learning::EvaluationMetric::BinaryErrorRate(0.5),
+            ]))
+            .build()
+            .unwrap();
+        let params = parameters::BoosterParametersBuilder::default()
+            .booster_type(parameters::BoosterType::Tree(tree_params))
+            .learning_params(learning_params)
+            .verbose(false)
+            .build()
+            .unwrap();
+        let mut booster =
+            Booster::new_with_cached_dmats(&params, &[&dmat_train, &dmat_test]).unwrap();
+
+        for i in 0..10 {
+            booster.update(&dmat_train, i).expect("update failed");
+        }
+
+        println!("ping");
+
+        let eps = 1e-6;
+
+        let train_metrics = booster.evaluate(&dmat_train).unwrap();
+        assert!(*train_metrics.get("logloss").unwrap() - 0.006634 < eps);
+        assert!(*train_metrics.get("map@4-").unwrap() - 0.001274 < eps);
+
+        let test_metrics = booster.evaluate(&dmat_test).unwrap();
+        assert!(*test_metrics.get("logloss").unwrap() - 0.00692 < eps);
+        assert!(*test_metrics.get("map@4-").unwrap() - 0.005155 < eps);
+
+        let v = booster.predict(&dmat_test).unwrap();
+        assert_eq!(v.len(), dmat_test.num_rows());
+
+        // first 10 predictions
+        let expected_start = [
+            0.0050151693,
+            0.9884467,
+            0.0050151693,
+            0.0050151693,
+            0.026636455,
+            0.11789363,
+            0.9884467,
+            0.01231471,
+            0.9884467,
+            0.00013656063,
+        ];
+
+        // last 10 predictions
+        let expected_end = [
+            0.002520344,
+            0.00060917926,
+            0.99881005,
+            0.00060917926,
+            0.00060917926,
+            0.00060917926,
+            0.00060917926,
+            0.9981102,
+            0.002855195,
+            0.9981102,
+        ];
+
+        for (pred, expected) in v.iter().zip(&expected_start) {
+            println!("predictions={}, expected={}", pred, expected);
+            assert!(pred - expected < eps);
+        }
+
+        for (pred, expected) in v[v.len() - 10..].iter().zip(&expected_end) {
+            println!("predictions={}, expected={}", pred, expected);
+            assert!(pred - expected < eps);
+        }
+    }
+
+    //     #[test]
+    //     fn dump_model() {
+    //         let dmat_train = DMatrix::load("xgboost-bib/xgboost/demo/data/agaricus.txt.train").unwrap();
+    //
+    //         println!("{:?}", dmat_train.shape());
+    //
+    //         let tree_params = tree::TreeBoosterParametersBuilder::default()
+    //             .max_depth(2)
+    //             .eta(1.0)
+    //             .build()
+    //             .unwrap();
+    //         let learning_params = learning::LearningTaskParametersBuilder::default()
+    //             .objective(learning::Objective::BinaryLogistic)
+    //             .build()
+    //             .unwrap();
+    //         let booster_params = parameters::BoosterParametersBuilder::default()
+    //             .booster_type(parameters::BoosterType::Tree(tree_params))
+    //             .learning_params(learning_params)
+    //             .verbose(false)
+    //             .build()
+    //             .unwrap();
+    //
+    //         let training_params = parameters::TrainingParametersBuilder::default()
+    //             .booster_params(booster_params)
+    //             .dtrain(&dmat_train)
+    //             .boost_rounds(10)
+    //             .build()
+    //             .unwrap();
+    //         let booster = Booster::train(&training_params).unwrap();
+    //
+    //         let features = FeatureMap::from_file("xgboost-bib/xgboost/demo/data/featmap.txt")
+    //             .expect("failed to parse feature map file");
+    //
+    //         assert_eq!(
+    //             booster.dump_model(true, Some(&features)).unwrap(),
+    //             "0:[odor=none] yes=2,no=1,gain=4000.53101,cover=1628.25
+    // 1:[stalk-root=club] yes=4,no=3,gain=1158.21204,cover=924.5
+    // 		3:leaf=1.71217716,cover=812
+    // 		4:leaf=-1.70044053,cover=112.5
+    // 2:[spore-print-color=green] yes=6,no=5,gain=198.173828,cover=703.75
+    // 		5:leaf=-1.94070864,cover=690.5
+    // 		6:leaf=1.85964918,cover=13.25
+    //
+    // 0:[stalk-root=rooted] yes=2,no=1,gain=832.545044,cover=788.852051
+    // 1:[odor=none] yes=4,no=3,gain=569.725098,cover=768.389709
+    // 		3:leaf=0.78471756,cover=458.936859
+    // 		4:leaf=-0.968530357,cover=309.45282
+    // 	2:leaf=-6.23624468,cover=20.462389
+    //
+    // 0:[ring-type=pendant] yes=2,no=1,gain=368.744568,cover=457.069458
+    // 1:[stalk-surface-below-ring=scaly] yes=4,no=3,gain=226.33696,cover=221.051468
+    // 		3:leaf=0.658725023,cover=212.999451
+    // 		4:leaf=5.77228642,cover=8.05200672
+    // 2:[spore-print-color=purple] yes=6,no=5,gain=258.184265,cover=236.018005
+    // 		5:leaf=-0.791407049,cover=233.487625
+    // 		6:leaf=-9.421422,cover=2.53038669
+    //
+    // 0:[odor=foul] yes=2,no=1,gain=140.486069,cover=364.119354
+    // 1:[gill-size=broad] yes=4,no=3,gain=139.860504,cover=274.101959
+    // 		3:leaf=0.614153326,cover=95.8599854
+    // 		4:leaf=-0.877905607,cover=178.241974
+    // 	2:leaf=1.07747853,cover=90.0174103
+    //
+    // 0:[spore-print-color=green] yes=2,no=1,gain=112.605011,cover=189.202194
+    // 1:[gill-spacing=close] yes=4,no=3,gain=66.4029999,cover=177.771835
+    // 		3:leaf=-1.26934469,cover=42.277401
+    // 		4:leaf=0.152607277,cover=135.494431
+    // 	2:leaf=2.92190909,cover=11.4303684
+    //
+    // 0:[odor=almond] yes=2,no=1,gain=52.5610275,cover=170.612762
+    // 1:[odor=anise] yes=4,no=3,gain=67.3869553,cover=150.881165
+    // 		3:leaf=0.431742132,cover=131.902222
+    // 		4:leaf=-1.53846073,cover=18.9789505
+    // 2:[gill-spacing=close] yes=6,no=5,gain=12.4420624,cover=19.731596
+    // 		5:leaf=-3.02413678,cover=3.65769386
+    // 		6:leaf=-1.02315068,cover=16.0739021
+    //
+    // 0:[odor=none] yes=2,no=1,gain=66.2389145,cover=142.360611
+    // 1:[odor=anise] yes=4,no=3,gain=31.2294312,cover=72.7557373
+    // 		3:leaf=0.777142286,cover=64.5309982
+    // 		4:leaf=-1.19710124,cover=8.22473907
+    // 2:[spore-print-color=green] yes=6,no=5,gain=12.1987419,cover=69.6048737
+    // 		5:leaf=-0.912605286,cover=66.1211166
+    // 		6:leaf=0.836115122,cover=3.48375821
+    //
+    // 0:[gill-size=broad] yes=2,no=1,gain=20.6531773,cover=79.4027634
+    // 1:[spore-print-color=white] yes=4,no=3,gain=16.0703697,cover=34.9289207
+    // 		3:leaf=-0.0180106498,cover=25.0319824
+    // 		4:leaf=1.4361918,cover=9.89693928
+    // 2:[odor=foul] yes=6,no=5,gain=22.1144333,cover=44.4738464
+    // 		5:leaf=-0.908311546,cover=36.982872
+    // 		6:leaf=0.890622675,cover=7.49097395
+    //
+    // 0:[odor=almond] yes=2,no=1,gain=11.7128553,cover=53.3251991
+    // 1:[ring-type=pendant] yes=4,no=3,gain=12.546154,cover=44.299942
+    // 		3:leaf=-0.515293062,cover=15.7899179
+    // 		4:leaf=0.56883812,cover=28.5100231
+    // 	2:leaf=-1.01502442,cover=9.02525806
+    //
+    // 0:[population=clustered] yes=2,no=1,gain=14.8892794,cover=45.9312019
+    // 1:[odor=none] yes=4,no=3,gain=10.1308851,cover=43.0564575
+    // 		3:leaf=0.217203051,cover=22.3283749
+    // 		4:leaf=-0.734555721,cover=20.7280827
+    // 2:[stalk-root=missing] yes=6,no=5,gain=19.3462334,cover=2.87474418
+    // 		5:leaf=3.63442755,cover=1.34154534
+    // 		6:leaf=-0.609474957,cover=1.53319895
+    // "
+    //         );
 }
